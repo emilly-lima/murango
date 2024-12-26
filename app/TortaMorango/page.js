8264ad56a887368f3bdd0257cb625c85d0a00841
Emilly Lima <97124187+emilly-lima@users.noreply.github.com>
2024-12-26 17:34:50 -0300

    add <a>

---
+++ app/TortaMorango/page.js
@@ -0,0 +1,10 @@
+"use client";
+import Header from "../components/header";
+export default function TortaMorango() {
+    return (
+        <div className="flex flex-row flex-wrap space-around" id="first">
+            <Header />
+            
+        </div>
+    );
+}
--- app/components/receitas.jsx
+++ app/components/receitas.jsx
@@ -1,65 +1,65 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from 'next/navigation';
 export default function Receitas() {
     const router = useRouter()
     return (
         <div className='grid grid-cols-2 gap-x-60 gap-y-80 space-between justify-center'>
-                <button id="card-receita" onClick={() => router.push('/BoloMorango')}>
-                    <Image
-                            src="/assets/default-image.jpg" 
-                            alt="Ativo 11"
-                            width={350} 
-                            height={100}
-                            className=''
-                            id="img-receita"
-                            />   
-                    <div id="title-receita">
-                    Bolo de Morango
-                    </div>         
-                </button>
+                <a href="/BoloMorango" id="card-receita">
+                        <Image
+                                src="/assets/default-image.jpg" 
+                                alt="Ativo 11"
+                                width={350} 
+                                height={100}
+                                className=''
+                                id="img-receita"
+                                />   
+                        <div id="title-receita">
+                        Bolo de Morango
+                        </div>      
+                </a>
+                <a href="/SorveteMorango" id="card-receita">
+                        <img
+                                    src="/assets/default-image.jpg" 
+                                    alt="Ativo 11"
+                                    width={350} 
+                                    height={100}
+                                    className=''
+                                    id="img-receita"
+                                    />   
+                            <div id="title-receita">
+                            Sorvete de Morango
+                            </div>
+                </a>
 
-                <div id="card-receita">
-                <Image
-                            src="/assets/default-image.jpg" 
-                            alt="Ativo 11"
-                            width={350} 
-                            height={100}
-                            className=''
-                            id="img-receita"
-                            />   
-                    <div id="title-receita">
-                    Sorvete de Morango
-                    </div>
-                </div>
-
-                <div id="card-receita">
-                <Image
-                            src="/assets/default-image.jpg" 
-                            alt="Ativo 11"
-                            width={350} 
-                            height={100}
-                            className=''
-                            id="img-receita"
-                            />   
-                    <div id="title-receita">
-                    Torta de Morango
-                    </div>
-                </div>
+                <a href="/TortaMorango" id="card-receita">
+                    <img
+                                src="/assets/default-image.jpg" 
+                                alt="Ativo 11"
+                                width={350} 
+                                height={100}
+                                className=''
+                                id="img-receita"
+                                />   
+                        <div id="title-receita">
+                        Torta de Morango
+                        </div>
+                </a>
                 
-                <div id="card-receita">
-                <Image
-                            src="/assets/default-image.jpg" 
-                            alt="Ativo 11"
-                            width={350} 
-                            height={100}
-                            className=''
-                            id="img-receita"
-                            />   
-                    <div id="title-receita">
-                    Escondidinho de Morango e Ninho
-                    </div>
-                </div>
+                <a href="/Escondidinho" id="card-receita">
+                    <Image
+                                src="/assets/default-image.jpg" 
+                                alt="Ativo 11"
+                                width={350} 
+                                height={100}
+                                className=''
+                                id="img-receita"
+                                />   
+                        <div id="title-receita">
+                        Escondidinho de Morango e Ninho
+                        </div>
+                </a>
         </div>
     )
 }
